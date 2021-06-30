46984b3d0d1ef4a8b93d48e0e914c80516301a5a
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-30 22:30:52 +0530

    [akhila]refactor:editing thr contacts in book

--- Addressbook.sql
+++ Addressbook.sql
@@ -15,14 +15,22 @@ Zipcode varchar(30),
 PhoneNumber varchar(20),
 )
 Alter Table Addressbook Add
-Email varchar(255);
+Email varchar(255) not null default 0;
 
 --UC3:-Inserting contacts to addressbook
 
 Insert into Addressbook(FirstName,LastName,Address,State,City,Zipcode,PhoneNumber,Email) VALUES
-  ('Akhila','reddy','gandhinagar','mumbai','delhi',509142,9666616881,'akhila@gmail.com'),  
-  ('Anu','Priya','hyd','karnataka','gadwal',590234,9908778909,'anuhya@gmail.com'),
-  ('Shamu','Varma','kurnool','Mumbai','gujarat',345678,8796543256,'priya@gmail.com');
-  
+  ('Akhila','reddy','gandhinagar','mumbai','delhi',509142,9666616881,'akhila.it@gmail.com'),  
+  ('Anu','Priya','hyd','karnataka','gadwal',590234,990877890,'anuhya@gmail.com'),
+  ('Shamu','Varma','kurnool','Mumbai','gujarat',345678,8796543256,'varma@gmail.com'),
+  ('Ramu','shamu','amaravati','Rajasthan','Pune',509234,7896543452,'ram@gmail.com');
+ 
+ select * from Addressbook 
 
-  select *from Addressbook
+ --UC4:-Editing the contacts in addressbook
+
+ Update Addressbook set state = 'maharastra' where FirstName = 'Akhila'
+
+ --UC5-Deleting the contacts in addressbook
+
+ delete from Addressbook where FirstName = 'Anu'
