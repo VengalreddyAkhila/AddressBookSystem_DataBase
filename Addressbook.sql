d23077c06c8f98adc9fa85c82297a50a52065c0a
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-30 13:34:38 +0530

    [akhila]Refactor:Create addressbook table

--- Addressbook.sql
+++ Addressbook.sql
@@ -2,3 +2,15 @@
 
 create database AdressBook
 use AdressBook
+
+--UC2:-Create Address book table 
+
+create table Addressbook(
+FirstName varchar(32),
+LastName varchar(32),
+Address varchar(32),
+State  varchar(255),
+City varchar(32),
+Zipcode varchar(30),
+PhoneNumber varchar(20)
+)
